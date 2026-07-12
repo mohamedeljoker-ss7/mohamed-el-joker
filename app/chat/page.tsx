47205ba51acb43a8364cc4ccf7c1c30904becea9
mohamedeljoker-ss7 <mhmdbdaltwabalsdawy7@gmail.com>
2026-07-12 20:01:21 +0300

    Create page.tsx

---
+++ app/chat/page.tsx
@@ -0,0 +1,16 @@
+export default function ChatPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#111827",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>💬 الشات (سيتم ربطه بـ Firebase في الخطوة القادمة)</h1>
+    </main>
+  );
+}
