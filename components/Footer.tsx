db0cfe74984c8c50280b0298598c6dc661a37226
mohamedeljoker-ss7 <mhmdbdaltwabalsdawy7@gmail.com>
2026-07-12 20:01:51 +0300

    Add Footer component with contact information

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#020617",
+        color: "#fff",
+        padding: "30px",
+        textAlign: "center",
+      }}
+    >
+      <h3>Joker Academy</h3>
+
+      <p>جميع الحقوق محفوظة © 2026</p>
+
+      <br />
+
+      <p>واتساب: +20 101 029 8878</p>
+
+      <p>تيليجرام: @Mo7amedEL_JOKER</p>
+    </footer>
+  );
+}
