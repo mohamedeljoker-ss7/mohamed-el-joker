7815ae0fe1ebb1efd06b65bf8eb1c638009c0ecc
mohamedeljoker-ss7 <mhmdbdaltwabalsdawy7@gmail.com>
2026-07-12 18:09:45 +0300

    Add NotFound component for 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+export default function NotFound() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#0f172a",
+        color: "white",
+      }}
+    >
+      <h1>404</h1>
+      <p>الصفحة غير موجودة</p>
+    </main>
+  );
+}
